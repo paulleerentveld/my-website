5101a40cf4c9f6a7a4fd09293cbfdd7bf5cf0d04
paulleerentveld <paul@veldnet.com.au>
2022-02-22 13:03:49 +1000

    changed photo layout and fixed up heaps of css styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by javascript";
-document.querySelector('body').appendChild(h2);
+const h4 = document.createElement('h4');
+h4.textContent = "This content added by javascript";
+document.querySelector('body').appendChild(h4);
 
 
