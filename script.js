1035a77343e548f6d28897fd5e00a476755b2016
paulleerentveld <paul@veldnet.com.au>
2022-02-28 11:04:50 +1000

    minor css changes

--- script.js
+++ script.js
@@ -1,5 +1,70 @@
-const h4 = document.createElement('h4');
-h4.textContent = "This content added by javascript";
-document.querySelector('body').appendChild(h4);
+
+// Half triangle
+
+/* 
+let n = 10;
+
+for(let i = 0; i<n;i++){
+	let row = ""
+	for(let j = 0; j<i+1; j++){
+		row = row + "*"
+	}
+	console.log(row);
+} */
+
+
+
+// Pyramid
+
+/* 
+let n = 10;
+
+for (let i = 0; i < n; i++) {
+    let string = "";
+    let k = (n*2)-1;
+    for (let j = 0; j <= k ; j++) {
+        if (j < n-i) {
+            string = string + " ";
+        } else if (j > n+i) {
+            string = string + " ";
+        } else {
+            string = string + "*";
+        }            
+    }
+    console.log(string);
+} */
+
+
+
+
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  //sayHelloTo('Isabel'); // "Hello, Isabel!"
+  
+
+/* 
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+    console.log("Greeting:", greeting);
+    console.log("firstName:", firstName);
+  } */
+
+  //say("good evening","Kimmy");
+
+  function add(x, y) {
+      return 10 * (x + y);
+  }
+
+  //console.log (add (5,5));
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`)
+  }
+say('Hello','Kimmy')
+
+
 
 
